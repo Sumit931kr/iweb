7313f9ca9622490f42bdaf41e2fa786590a5ea66
Sumit <sumitkumar280601@gmail.com>
2022-03-13 16:56:36 +0530

    third commit

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
 
-const socket = io('http://localhost:8000');
+const socket = io('https://iwebchat.herokuapp.com/');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
